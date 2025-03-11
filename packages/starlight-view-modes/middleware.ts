4ce538c8390b9024c03c1f85881b56ee5f9ced20
trueberryless <trueberryless+github@gmail.com>
2025-03-11 19:28:31 +0100

    feat: site title href stays in current mode

--- packages/starlight-view-modes/middleware.ts
+++ packages/starlight-view-modes/middleware.ts
@@ -4,11 +4,14 @@ import { currentModeKey } from "./libs/shared";
 
 export const onRequest = defineRouteMiddleware((context) => {
   const { starlightRoute } = context.locals;
-  const { id, sidebar, pagination } = starlightRoute;
+  const { id, sidebar, pagination, siteTitleHref } = starlightRoute;
 
   const currentMode = getCurrentMode(id, sidebar, pagination);
   starlightRoute.sidebar = currentMode.sidebar;
   starlightRoute.pagination = currentMode.pagination;
 
   starlightRoute[currentModeKey] = currentMode.mode;
+
+  if (currentMode.mode !== "default")
+    starlightRoute.siteTitleHref = `/${currentMode.mode}${siteTitleHref}`;
 });
